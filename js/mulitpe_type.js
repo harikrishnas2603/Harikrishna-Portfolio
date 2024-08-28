763a2a7a02e22ecb94ec6a914f0b2fb2d72232f3
HARIKRISHNA <142434562+harikrishnas2603@users.noreply.github.com>
2024-08-28 22:20:26 +0530

    Add files via upload

---
+++ js/mulitpe_type.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+  strings: ['Web Developer', 'App Developer','Data Analyst'],
+  typeSpeed: 150,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop:true
+});
